911be4b0551f5d1e5cd33d0dc3ba17e994c6db39
DanielaRossi <89362736+DanielaRossi@users.noreply.github.com>
2021-08-25 20:53:57 -0300

    RepasoPrimeraClase
    Repaso primera clase

---
+++ Cuatri.2021.PrimeraClase/Cuatri.2021.PrimeraClase.Consola/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cuatri2021.PrimeraClase.Validación;
+
+namespace Cuatri._2021.PrimeraClase.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Ingrese un numero: ");
+            string Ingresousuario = Console.ReadLine();
+            int NumeroIngresado = Convert.ToInt32(Ingresousuario);
+            bool esValido= Validador.EsValido(NumeroIngresado);
+
+            if (esValido)
+            {
+                Console.WriteLine("Es valido.");
+            }
+            else{
+                Console.WriteLine("No es valido.");
+
+            }
+        }
+    }
+}
---
+++ Cuatri2021.PrimeraClase.Validación/Validador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuatri2021.PrimeraClase.Validación
+{
+    class Validador
+    {
+        public bool EsValido(int numero)
+        {
+            if (numero == 2)
+                return true;
+            else
+                return false;
+        }
+    }
+}
