14b1987c54f0003e657d3315705859eb0482052d
DanielaRossi <89362736+DanielaRossi@users.noreply.github.com>
2021-08-31 20:38:26 -0300

    Ejercicios
    Ejercicios

---
+++ POO. Almacen/POO.almacen.biblioteca/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.almacen.biblioteca
+{
+    public class Producto
+    {
+        //atributos de clase
+        private string _nombreProducto;
+        private int _idProducto;
+        private int _stockProducto;
+        private float _precioProducto;
+        private string _marcaProducto;
+        
+    }
+}
