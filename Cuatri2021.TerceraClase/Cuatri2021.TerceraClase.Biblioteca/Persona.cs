911046ea8ec6edacb035fa444f20594b00602bcb
DanielaRossi <89362736+DanielaRossi@users.noreply.github.com>
2021-09-04 15:28:10 -0300

    ejercicios
    ejercicios

---
+++ Cuatri2021.TerceraClase/Cuatri2021.TerceraClase.Biblioteca/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuatri2021.TerceraClase.Biblioteca
+{
+    public class Persona
+    {
+        //atributos
+        private string _nombre;
+        private int _modificacionNombre;
+        private int _porcentajeCansancio;
+        //propiedad
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                if(value!=string.Empty)
+                    {
+                    _nombre = value;
+                    _modificacionNombre++;
+                }
+                
+            }
+        }
+        public int PorcentajeEstadoFisico
+        {
+            get
+            {
+                return _porcentajeCansancio;
+            }
+        }
+    }
+}
---
+++ Cuatri2021.TerceraClase/Cuatri2021.TerceraClase.Consola/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cuatri2021.TerceraClase.Biblioteca;
+
+
+namespace Cuatri2021.TerceraClase.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona();
+            p1.Nombre = "Daniela";
+            Console.WriteLine(p1.Nombre);
+        }
+    }
+}
